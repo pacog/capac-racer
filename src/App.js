18ddcb174d0124353f8cf7dd947e87c5cc3ab1f4
Paco Gomez Granados <pacogomezgranados@gmail.com>
2019-07-30 16:55:16 +0200

    Button to pause

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Countdown from 'components/Countdown';
 import { CounterContext } from 'contexts/counter';
 import Counter from 'utils/Counter';
@@ -7,13 +7,25 @@ import './App.css';
 const counter = new Counter();
 
 function App() {
+    const [isPaused, setPaused] = useState(false);
+
     return (
         <CounterContext.Provider value={counter}>
             <div className="App">
                 <header className="App-header">
                     <CounterContext.Consumer>
-                        {(value) => <Countdown counter={value} />}
+                        {(value) => (
+                            <Countdown counter={value} isPaused={isPaused} />
+                        )}
                     </CounterContext.Consumer>
+                    <button
+                        type="button"
+                        onClick={() => {
+                            setPaused(!isPaused);
+                        }}
+                    >
+                        {isPaused ? 'Unpause' : 'pause'}
+                    </button>
                 </header>
             </div>
         </CounterContext.Provider>
