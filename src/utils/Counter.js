98a68e71c60a577c69b83419cd9ad2e54a5778ed
Paco Gomez Granados <pacogomezgranados@gmail.com>
2019-07-30 19:41:27 +0200

    Private fields in counter

--- src/utils/Counter.js
+++ src/utils/Counter.js
@@ -2,57 +2,59 @@ import { keepRequestingFrames } from 'utils/keepRequestingFrames';
 
 export default class Counter {
     constructor({ timeLimit }) {
-        this.timeLimit = timeLimit;
+        this.#timeLimit = timeLimit;
     }
 
-    paused = false;
+    #timeLimit = null;
 
-    lastTimeStamp = null;
+    #paused = false;
 
-    elapsedTime = 0;
+    #lastTimeStamp = null;
 
-    onEachFrame = (timestamp) => {
-        if (!this.lastTimeStamp) {
-            this.lastTimeStamp = timestamp;
+    #elapsedTime = 0;
+
+    #onEachFrame = (timestamp) => {
+        if (!this.#lastTimeStamp) {
+            this.#lastTimeStamp = timestamp;
         }
-        if (!this.paused) {
-            const timeSinceLastTimestamp = timestamp - this.lastTimeStamp;
-            this.elapsedTime = this.elapsedTime + timeSinceLastTimestamp;
+        if (!this.#paused) {
+            const timeSinceLastTimestamp = timestamp - this.#lastTimeStamp;
+            this.#elapsedTime = this.#elapsedTime + timeSinceLastTimestamp;
             if (this.getRemainingTime() === 0) {
                 this.pause();
-                this.onEndCallbacks.forEach((callback) => callback());
+                this.#onEndCallbacks.forEach((callback) => callback());
             }
         }
-        this.lastTimeStamp = timestamp;
+        this.#lastTimeStamp = timestamp;
     };
 
-    onEndCallbacks = [];
+    #onEndCallbacks = [];
 
-    destroyer = keepRequestingFrames(this.onEachFrame);
+    #destroyer = keepRequestingFrames(this.#onEachFrame);
 
     pause() {
-        this.paused = true;
+        this.#paused = true;
     }
 
     unpause() {
-        this.paused = false;
+        this.#paused = false;
     }
 
     restart() {
-        this.elapsedTime = 0;
+        this.#elapsedTime = 0;
         this.unpause();
     }
 
     onEnd(callback) {
-        this.onEndCallbacks = this.onEndCallbacks.concat([callback]);
+        this.#onEndCallbacks = this.#onEndCallbacks.concat([callback]);
     }
 
     getRemainingTime() {
-        const remaining = this.timeLimit - this.elapsedTime;
+        const remaining = this.#timeLimit - this.#elapsedTime;
         return Math.max(remaining, 0);
     }
 
     destroy() {
-        this.destroyer();
+        this.#destroyer();
     }
 }
