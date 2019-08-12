ec344049ae06f5f3ea2e963dbd2c36fa06522e99
Paco Gomez Granados <pacogomezgranados@gmail.com>
2019-08-12 22:44:42 +0200

    Adding Redux dev tools

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import rootReducer from 'store/rootReducer';
 import Router from './Router';
 import './App.css';
 
-const store = createStore(rootReducer);
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk)),
+);
 
 function App() {
     return (
