7d1fa54ce9e5f844ad5fbd2729294f794b531b7a
Paco Gomez Granados <pacogomezgranados@gmail.com>
2019-09-05 10:33:19 +0200

    Manual move of camera

--- src/components/GameBoard/index.jsx
+++ src/components/GameBoard/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { useState } from 'react';
 import Grid from 'components/Grid';
 import Player from 'components/Player';
 import { useSelector, useDispatch } from 'react-redux';
@@ -13,8 +14,12 @@ import PlayerTrail from 'components/PlayerTrail';
 import CounterDisplay from 'components/CounterDisplay';
 import waitingForPlayerCounter from 'utils/waitingForPlayerCounter';
 import './style.css';
+import { substract, add } from 'utils/vector2d';
 
 const GameBoard = () => {
+    const [cameraPosition, setCameraPosition] = useState({ x: 0, y: 0 });
+    const [isDragging, setIsDragging] = useState(false);
+    const [dragStart, setDragStart] = useState(null);
     const dispatch = useDispatch();
     const players = useSelector((state) => getAllPlayers(state));
     const currentPlayer = useSelector((state) => getCurrentPlayer(state));
@@ -28,42 +33,80 @@ const GameBoard = () => {
     return (
         <div className="game-board">
             <div
-                className="game-board-circuit-bg"
+                className="game-board-content"
                 style={{
-                    backgroundImage: `url(${circuitInfo.bgImg})`,
-                    backgroundSize: `${circuitInfo.width}px ${circuitInfo.height}px`,
+                    width: circuitInfo.width,
+                    height: circuitInfo.height,
+                    transform: `translate(${cameraPosition.x}px, ${cameraPosition.y}px)`,
                 }}
-            />
-            <Grid zoom={mapZoom} cellSize={gridSize} />
-            {players.map((player) => (
-                <div key={player.id}>
-                    <Player
-                        player={player}
-                        isActive={player === currentPlayer}
-                    />
-                    <PlayerTrail
-                        player={player}
-                        isActive={player === currentPlayer}
-                    />
-                </div>
-            ))}
-            {currentPlayer && waitingForPlayerInput && (
-                <>
-                    <CounterDisplay counterObject={waitingForPlayerCounter} />
-                    <MovementPicker
-                        player={currentPlayer}
-                        otherPlayers={players.filter(
-                            (otherPlayer) =>
-                                otherPlayer.id !== currentPlayer.id,
-                        )}
-                        onPositionSelected={(position) =>
-                            dispatch(
-                                handlePlayerMovement(currentPlayer, position),
-                            )
-                        }
-                    />
-                </>
-            )}
+                onMouseDown={(event) => {
+                    setIsDragging(true);
+                    setDragStart({
+                        x: event.clientX,
+                        y: event.clientY,
+                    });
+                }}
+                onMouseUp={() => {
+                    setIsDragging(false);
+                    setDragStart(null);
+                }}
+                onMouseMove={(event) => {
+                    if (!isDragging || !dragStart) {
+                        return;
+                    }
+
+                    const cursorPosition = {
+                        x: event.clientX,
+                        y: event.clientY,
+                    };
+                    const movement = substract(cursorPosition, dragStart);
+                    setDragStart(cursorPosition);
+                    setCameraPosition(add(cameraPosition, movement));
+                }}
+            >
+                <div
+                    className="game-board-circuit-bg"
+                    style={{
+                        backgroundImage: `url(${circuitInfo.bgImg})`,
+                        backgroundSize: `${circuitInfo.width}px ${circuitInfo.height}px`,
+                    }}
+                />
+                <Grid zoom={mapZoom} cellSize={gridSize} />
+                {players.map((player) => (
+                    <div key={player.id}>
+                        <Player
+                            player={player}
+                            isActive={player === currentPlayer}
+                        />
+                        <PlayerTrail
+                            player={player}
+                            isActive={player === currentPlayer}
+                        />
+                    </div>
+                ))}
+                {currentPlayer && waitingForPlayerInput && (
+                    <>
+                        <CounterDisplay
+                            counterObject={waitingForPlayerCounter}
+                        />
+                        <MovementPicker
+                            player={currentPlayer}
+                            otherPlayers={players.filter(
+                                (otherPlayer) =>
+                                    otherPlayer.id !== currentPlayer.id,
+                            )}
+                            onPositionSelected={(position) =>
+                                dispatch(
+                                    handlePlayerMovement(
+                                        currentPlayer,
+                                        position,
+                                    ),
+                                )
+                            }
+                        />
+                    </>
+                )}
+            </div>
         </div>
     );
 };
