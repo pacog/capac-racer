011d2ffaa2d82927efa59b9e253ecb5523f6ab3e
Paco Gomez Granados <pacogomezgranados@gmail.com>
2019-09-10 12:31:29 +0200

    Added new more simple circuit

--- src/screens/MainMenu/index.jsx
+++ src/screens/MainMenu/index.jsx
@@ -8,49 +8,49 @@ const hardcodedPlayers = [
         id: '1',
         name: 'Player One',
         position: {
-            x: 5,
-            y: 25,
-        },
-        speed: {
-            x: -1,
-            y: -2,
-        },
-        prevPositions: [
-            {
-                x: 5,
-                y: 25,
-            },
-        ],
-        style: BLUE,
-    },
-    {
-        id: '2',
-        name: 'Player Dos',
-        position: {
             x: 6,
-            y: 25,
+            y: 15,
         },
         speed: {
-            x: -1,
-            y: -2,
+            x: 1,
+            y: 0,
         },
         prevPositions: [
             {
                 x: 6,
-                y: 25,
+                y: 15,
             },
         ],
-        style: GREEN,
+        style: BLUE,
     },
+    // {
+    //     id: '2',
+    //     name: 'Player Dos',
+    //     position: {
+    //         x: 6,
+    //         y: 25,
+    //     },
+    //     speed: {
+    //         x: -1,
+    //         y: -2,
+    //     },
+    //     prevPositions: [
+    //         {
+    //             x: 6,
+    //             y: 25,
+    //         },
+    //     ],
+    //     style: GREEN,
+    // },
 ];
 
-const hardcodedOrder = ['2', '1'];
+const hardcodedOrder = ['1'];
 const hardcodedCircuit = {
-    collisionImg: `${process.env.PUBLIC_URL}circuits/circuit1/collision.png`,
-    bgImg: `${process.env.PUBLIC_URL}circuits/circuit1/bg.png`,
-    checkpoint1: `${process.env.PUBLIC_URL}circuits/circuit1/checkpoint1.png`,
-    checkpoint2: `${process.env.PUBLIC_URL}circuits/circuit1/checkpoint2.png`,
-    checkpoint3: `${process.env.PUBLIC_URL}circuits/circuit1/checkpoint3.png`,
+    collisionImg: `${process.env.PUBLIC_URL}circuits/circuit2/collision.png`,
+    bgImg: `${process.env.PUBLIC_URL}circuits/circuit2/bg.png`,
+    checkpoint1: `${process.env.PUBLIC_URL}circuits/circuit2/checkpoint_1.png`,
+    checkpoint2: `${process.env.PUBLIC_URL}circuits/circuit2/checkpoint_2.png`,
+    checkpoint3: `${process.env.PUBLIC_URL}circuits/circuit2/checkpoint_3.png`,
 };
 
 const MainMenu = () => {
