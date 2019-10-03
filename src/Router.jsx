b207ce99e486cb34255aea307b9caaa6a4220cb5
Paco Gomez Granados <pacogomezgranados@gmail.com>
2019-10-03 12:30:07 +0200

    Improved route transition handling and start styling

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,46 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { TransitionGroup, Transition } from 'react-transition-group';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import * as screens from 'constants/screens';
 import MainMenu from 'screens/MainMenu';
 import Game from 'screens/Game';
 import CircuitSelection from 'screens/CircuitSelection';
 import PlayersSelection from 'screens/PlayersSelection';
 
+const TIME_TO_SHOW_FIRST_SCREEN = 500; // ms
 const STATE_CHANGE_TRANSITION_TIME = 3000; // ms
 
 function Router() {
-    const screenId = useSelector((state) => state.mainUI.currentScreen);
+    const [gameReady, setGameReady] = useState(false);
+    const currentScreenName = useSelector(
+        (state) => state.mainUI.currentScreen,
+    );
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setGameReady(true);
+        }, TIME_TO_SHOW_FIRST_SCREEN);
+        return () => clearTimeout(timeout);
+    }, []);
+
+    const allScreens = [
+        {
+            screenName: screens.MAIN_MENU,
+            Component: MainMenu,
+        },
+        {
+            screenName: screens.CIRCUIT_SELECTION,
+            Component: CircuitSelection,
+        },
+        {
+            screenName: screens.PLAYERS_SELECTION,
+            Component: PlayersSelection,
+        },
+        {
+            screenName: screens.GAME,
+            Component: Game,
+        },
+    ];
 
     return (
         <TransitionGroup className="menu-screen full-screen">
-            {screenId === screens.MAIN_MENU && (
-                <Transition timeout={STATE_CHANGE_TRANSITION_TIME}>
-                    {(state) => <MainMenu className={`fade fade-${state}`} />}
-                </Transition>
-            )}
-
-            {screenId === screens.CIRCUIT_SELECTION && (
-                <Transition timeout={STATE_CHANGE_TRANSITION_TIME}>
-                    {(state) => (
-                        <CircuitSelection className={`fade fade-${state}`} />
-                    )}
-                </Transition>
-            )}
-
-            {screenId === screens.PLAYERS_SELECTION && (
-                <Transition timeout={STATE_CHANGE_TRANSITION_TIME}>
-                    {(state) => (
-                        <PlayersSelection className={`fade fade-${state}`} />
-                    )}
-                </Transition>
-            )}
-
-            {screenId === screens.GAME && (
-                <Transition timeout={STATE_CHANGE_TRANSITION_TIME}>
-                    {(state) => <Game className={`fade fade-${state}`} />}
-                </Transition>
-            )}
+            {allScreens
+                .filter(
+                    ({ screenName }) =>
+                        gameReady && currentScreenName === screenName,
+                )
+                .map(({ screenName, Component }) => (
+                    <CSSTransition
+                        key={screenName.toString()}
+                        timeout={STATE_CHANGE_TRANSITION_TIME}
+                        classNames="fade"
+                    >
+                        <Component />
+                    </CSSTransition>
+                ))}
         </TransitionGroup>
     );
 }
