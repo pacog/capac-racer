49d6b3dbb4d9a07c45689ad7ef22609a93c8438d
Paco Gomez Granados <pacogomezgranados@gmail.com>
2020-01-07 16:10:08 +0100

    More URLS fixes

--- src/components/CircuitSelector/index.jsx
+++ src/components/CircuitSelector/index.jsx
@@ -9,7 +9,7 @@ function CircuitSelector({ circuit, onClick }) {
         <div className="circuit-selector" onClick={onClick}>
             <img
                 className="circuit-selector-img"
-                src={`${process.env.PUBLIC_URL}${circuit.bgImg}`}
+                src={circuit.bgImg}
                 alt={circuit.name}
             />
             <div className="circuit-selector-footer">
