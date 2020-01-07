bc67c2b61d319a1ea67009357a6b1b6d53f67ad8
Paco Gomez Granados <pacogomezgranados@gmail.com>
2020-01-07 15:39:58 +0100

    Updates

---
+++ precache-manifest.ffa6b9c9be3acf9a47148b6ec77da2ca.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "844894fa012b381701f650eb0bf199d7",
+    "url": "/capac-racer/index.html"
+  },
+  {
+    "revision": "9753e6068199e283289e",
+    "url": "/capac-racer/static/css/main.f0849048.chunk.css"
+  },
+  {
+    "revision": "cebb18cac5d3f3f4801d",
+    "url": "/capac-racer/static/js/2.49e3a265.chunk.js"
+  },
+  {
+    "revision": "00dfd96ba9057edeaeea7455171bb2f6",
+    "url": "/capac-racer/static/js/2.49e3a265.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9753e6068199e283289e",
+    "url": "/capac-racer/static/js/main.9c6e82e9.chunk.js"
+  },
+  {
+    "revision": "0d0f1a32045196fb4d3a",
+    "url": "/capac-racer/static/js/runtime-main.60e2173a.js"
+  },
+  {
+    "revision": "948b2ce7dcd935716bd32daf54503d68",
+    "url": "/capac-racer/static/media/destroy_-webfont.948b2ce7.woff2"
+  },
+  {
+    "revision": "fdff41831653931ff57b718d73d4d550",
+    "url": "/capac-racer/static/media/destroy_-webfont.fdff4183.woff"
+  },
+  {
+    "revision": "22002cb9efe8c3afbcc1557967edfcad",
+    "url": "/capac-racer/static/media/doppioone-regular-webfont.22002cb9.woff"
+  },
+  {
+    "revision": "3d276e826b1a5757654b9274e217b83e",
+    "url": "/capac-racer/static/media/doppioone-regular-webfont.3d276e82.woff2"
+  }
+]);
