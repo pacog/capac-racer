f891eb79b1f3f06e19207dfb5ef62fd0b45e81eb
Paco Gomez Granados <pacogomezgranados@gmail.com>
2020-01-28 11:26:04 +0100

    First tutorial screen

---
+++ src/screens/Tutorial/TutorialScreen0.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function TutorialScreen0({ onNext }) {
+    return (
+        <div className="tutorial-screen">
+            <div className="tutorial-screen-image">Image</div>
+            <div className="tutorial-screen-text">
+                <div className="tutorial-screen-text-filler">
+                    <p>This is your car.</p>
+                </div>
+                <button
+                    className="button tutorial-screen-button"
+                    type="button"
+                    onClick={onNext}
+                >
+                    Next
+                </button>
+            </div>
+        </div>
+    );
+}
+
+TutorialScreen0.propTypes = {
+    onNext: PropTypes.func.isRequired,
+};
+
+export default TutorialScreen0;
--- src/screens/Tutorial/index.jsx
+++ src/screens/Tutorial/index.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames';
 import { useDispatch } from 'react-redux';
 import { MAIN_MENU } from 'constants/screens';
 import Logo from 'components/Logo';
 import { changeScreen } from 'store/main-ui/actions';
+import TutorialScreen0 from './TutorialScreen0';
 import './style.css';
 
 function Tutorial({ className }) {
     const dispatch = useDispatch();
+    const [currentScreen, setCurrentScreen] = useState(0);
+    const goToNextScreen = () => {
+        setCurrentScreen(currentScreen + 1);
+    };
+
     return (
         <div
             className={classNames(
@@ -19,7 +25,11 @@ function Tutorial({ className }) {
                 <Logo variant="small" />
                 <h1 className="menu-header-title">How to play</h1>
             </div>
-            <div className="menu-content transition-from-right">Tutorial</div>
+            <div className="menu-content transition-from-right tutorial-screen-container">
+                {currentScreen === 0 && (
+                    <TutorialScreen0 onNext={goToNextScreen} />
+                )}
+            </div>
 
             <footer className="menu-footer transition-from-right">
                 <button
