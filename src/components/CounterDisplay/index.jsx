2b23f5e21540fdd62b2dc10bc03dc8761d470304
Paco Gomez Granados <pacogomezgranados@gmail.com>
2020-03-19 13:17:20 +0100

    Counter display to component

--- src/components/CounterDisplay/index.jsx
+++ src/components/CounterDisplay/index.jsx
@@ -2,33 +2,41 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './style.css';
 
-function CounterDisplay({ counterObject }) {
-    const [timeLeft, setTimeLeft] = React.useState(0);
-    // Use useRef for mutable variables that we want to persist
-    // without triggering a re-render on their change
-    const requestRef = React.useRef();
-
-    const animate = () => {
-        setTimeLeft(counterObject.getTimeLeft());
-        // @ts-ignore
-        requestRef.current = requestAnimationFrame(animate);
+class CounterDisplay extends React.Component {
+    state = {
+        timeLeft: 0,
     };
 
-    React.useEffect(() => {
-        // @ts-ignore
-        requestRef.current = requestAnimationFrame(animate);
-        return () => cancelAnimationFrame(requestRef.current);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []); // Make sure the effect runs only once
+    /** @type {number} */
+    requestRef = null;
 
-    return (
-        <div className="counter-display">
-            <div className="counter-display-text">Time left:</div>
-            <div className="counter-display-number">
-                {(timeLeft / 1000).toFixed(3)}
+    componentDidMount() {
+        this.animate();
+    }
+
+    componentWillUnmount() {
+        if (this.requestRef) {
+            cancelAnimationFrame(this.requestRef);
+        }
+    }
+
+    animate = () => {
+        this.setState({
+            timeLeft: this.props.counterObject.getTimeLeft(),
+        });
+        this.requestRef = requestAnimationFrame(this.animate);
+    };
+
+    render() {
+        return (
+            <div className="counter-display">
+                <div className="counter-display-text">Time left:</div>
+                <div className="counter-display-number">
+                    {(this.state.timeLeft / 1000).toFixed(3)}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 CounterDisplay.propTypes = {
